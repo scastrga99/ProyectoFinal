45d900af06b731612b2135750f3ef37a488374c1
scastrga99 <samuelcastrogarciagm@gmail.com>
2025-02-02 11:13:43 +0100

    Se añaden las vistas home, error y profesores

--- src/main/java/com/samuelcastro/ProyectoFinal/controllers/HomeController.java
+++ src/main/java/com/samuelcastro/ProyectoFinal/controllers/HomeController.java
@@ -1,16 +1,14 @@
 package com.samuelcastro.ProyectoFinal.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    
-        @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("message", "Hello, World!");
-        return "home";
+
+    @GetMapping("/")
+    public String home() {
+        return "index.html"; // Asegúrate de que la vista "index.html" esté en la carpeta "templates"
     }
 
 }
